d769f6c67edf38de0605388b3289a613da837bef
agabrail <agabrail@ltu.edu>
2024-01-23 15:43:34 -0500

    Moved The New Project To This Repo

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            StartStopBtn = new Button();
+            VideoPictureBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)VideoPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // StartStopBtn
+            // 
+            StartStopBtn.Location = new Point(874, 667);
+            StartStopBtn.Name = "StartStopBtn";
+            StartStopBtn.Size = new Size(188, 58);
+            StartStopBtn.TabIndex = 0;
+            StartStopBtn.Text = "Start";
+            StartStopBtn.UseVisualStyleBackColor = true;
+            StartStopBtn.Click += button1_Click;
+            // 
+            // VideoPictureBox
+            // 
+            VideoPictureBox.Location = new Point(192, 81);
+            VideoPictureBox.Name = "VideoPictureBox";
+            VideoPictureBox.Size = new Size(690, 464);
+            VideoPictureBox.TabIndex = 1;
+            VideoPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1133, 769);
+            Controls.Add(VideoPictureBox);
+            Controls.Add(StartStopBtn);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)VideoPictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button StartStopBtn;
+        private PictureBox VideoPictureBox;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,138 @@
+
+using Emgu.CV;
+
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+      
+
+        VideoCapture mCapture;
+
+       Thread mCaptureThread;
+
+        CancellationTokenSource mCancellationToken = new();
+
+        bool mIsCapturing = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                mCapture = new VideoCapture(0);
+
+                if (mCapture.Height == 0)
+                    throw new Exception("No Cameras Found)");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+            //Dispose all processing threads to avoid orphaned processes
+            if (mIsCapturing)
+            {
+                mCancellationToken.Cancel();
+
+            }
+            mCapture.Dispose();
+            mCancellationToken.Dispose();
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (mIsCapturing) // if cam is live, stop it
+            {
+
+                mCancellationToken.Cancel(); //request stop
+                mIsCapturing = false; //indicate new state
+
+                StartStopBtn.Text = "Start";
+
+
+            }
+            else // if it is dead start it
+            {
+
+                mCancellationToken = new(); //reinitalize
+                //Initalize new thread 
+
+                mCaptureThread = new(() => DisplayWebcam(mCancellationToken.Token));
+
+                mCaptureThread.Start();//start it
+
+                mIsCapturing = true; // Change state
+
+                StartStopBtn.Text = "Stop";
+
+
+
+
+            }
+
+
+
+        }
+
+        private void DisplayWebcam(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)//White no requested cancellation
+            {
+
+
+                Mat frame = mCapture.QueryFrame(); //grab a new frame
+                //resize to picturebox aspect ratio
+                int newHeight = (frame.Size.Height * VideoPictureBox.Size.Width) / frame.Size.Width;
+                Size newSize = new Size(VideoPictureBox.Size.Width, newHeight);
+                CvInvoke.Resize(frame, frame, newSize);
+
+                //~60fps -> 1000ms/60 = 16.6
+
+                Task.Delay(16);
+
+                VideoPictureBox.Image = frame.ToBitmap(); //Display current frame
+
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+}
