03956eb59334a831909d76a77147a5b2040515e3
agabrail <agabrail@ltu.edu>
2024-01-23 14:31:44 -0500

    Button stuff

--- Form1.cs
+++ Form1.cs
@@ -10,16 +10,16 @@ namespace UR1_Anthony_Gabrail
 
         Thread mCaptureThread;
 
-        CancellationTokenSource m;
-        private Button StartStopButton;
+        CancellationTokenSource mCancellationToken = new();
         private PictureBox VideoPictureBox;
+        private Button StartStopBtn;
         bool mIsCapturing = false;
 
-        public Form1()
-        {
+       // public Form1()
+        //{
 
-            InitializeComponent();
-        }
+        //    InitializeComponent();
+      //  }
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -40,22 +40,64 @@ namespace UR1_Anthony_Gabrail
 
         }
 
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+            //Dispose all processing threads to avoid orphaned processes
+            if (mIsCapturing)
+            {
+                mCancellationToken.Cancel();
+
+            }
+            mCapture.Dispose();
+            mCancellationToken.Dispose();
+
+        }
+
+
+        private void StartStopBtn_Click(object sender, EventArgs e)
+        {
+
+            if (mIsCapturing) // if cam is live, stop it
+            {
+
+                mCancellationToken.Cancel(); //request stop
+                mIsCapturing = false; //indicate new state
+                StartStopBtn.Text = "Start";
+
+
+            }
+            else // if it is dead start it
+            {
+
+                mCancellationToken = new(); //reinitalize
+                //Initalize new thread 
+
+                mCaptureThread = new(() => DisplayWebcam(mCancellationToken.Token));
+
+                mCaptureThread.Start();//start it
+
+                mIsCapturing = true; // Change state
+
+                StartStopBtn.Text = "Stop";
+
+
+
+
+            }
+
+
+        }
+
+
+
+
         private void InitializeComponent()
         {
-            StartStopButton = new Button();
             VideoPictureBox = new PictureBox();
+            StartStopBtn = new Button();
             ((System.ComponentModel.ISupportInitialize)VideoPictureBox).BeginInit();
             SuspendLayout();
-            // 
-            // StartStopButton
-            // 
-            StartStopButton.Location = new Point(1010, 748);
-            StartStopButton.Name = "StartStopButton";
-            StartStopButton.Size = new Size(188, 58);
-            StartStopButton.TabIndex = 0;
-            StartStopButton.Text = "Start";
-            StartStopButton.UseVisualStyleBackColor = true;
-            StartStopButton.Click += button1_Click;
             // 
             // VideoPictureBox
             // 
@@ -65,19 +107,52 @@ namespace UR1_Anthony_Gabrail
             VideoPictureBox.TabIndex = 1;
             VideoPictureBox.TabStop = false;
             // 
+            // StartStopBtn
+            // 
+            StartStopBtn.Location = new Point(374, 820);
+            StartStopBtn.Name = "StartStopBtn";
+            StartStopBtn.Size = new Size(188, 58);
+            StartStopBtn.TabIndex = 2;
+            StartStopBtn.Text = "Start";
+            StartStopBtn.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             ClientSize = new Size(1340, 942);
+            Controls.Add(StartStopBtn);
             Controls.Add(VideoPictureBox);
-            Controls.Add(StartStopButton);
             Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)VideoPictureBox).EndInit();
             ResumeLayout(false);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void DisplayWebcam(CancellationToken token)
         {
+            while (!token.IsCancellationRequested)//White no requested cancellation
+            {
+
+
+                Mat frame = mCapture.QueryFrame(); //grab a new frame
+                //resize to picturebox aspect ratio
+                int newHeight = (frame.Size.Height * VideoPictureBox.Size.Width) / frame.Size.Width;
+                Size newSize = new Size(VideoPictureBox.Size.Width, newHeight);
+                CvInvoke.Resize(frame, frame, newSize);
+
+                //~60fps -> 1000ms/60 = 16.6
+
+                Task.Delay(16);
+
+                VideoPictureBox.Image = frame.ToBitmap(); //Display current frame
+
+
+
+            }
+
+
+
         }
+
     }
+
 
 }
